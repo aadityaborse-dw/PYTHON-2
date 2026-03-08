4250b7aba5c67ad373b6f0a70a541bb50d0674c1
Aaditya borse <218811864+aadityaborse-dw@users.noreply.github.com>
2026-03-09 00:34:11 +0530

    methodoverloading.py

--- methodoverloading.py
+++ methodoverloading.py
@@ -2,6 +2,6 @@ class calculator:
     def add(self,a,b,c):
         return a+b+c
 cal=calculator()
-print(cal.add(9,0,0))
-print(cal.add(20,40,0))
+print(cal.add(8,0,0))
+print(cal.add(30,50,0))
 print(cal.add(70,50,20))
