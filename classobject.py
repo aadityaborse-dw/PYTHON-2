c019df0040b5023756c792428d9bc0c151b46577
Aaditya borse <218811864+aadityaborse-dw@users.noreply.github.com>
2026-03-09 00:33:20 +0530

    classobject.py

--- classobject.py
+++ classobject.py
@@ -5,8 +5,8 @@ class Car:
        self.year = year
     def display_info(self):
         print(self.brand, self.model, self.year)
-c1=Car("MARUTI SUZUKI", "BALENO", 2024)
-c2=Car("MAHINDRA", "XUV700", 2020)
+c1=Car("KOENISEGG", "JESKO", 2024)
+c2=Car("MASERATI", "GRANTURISMO", 2020)
 c1.display_info()
 
 c2.display_info()
